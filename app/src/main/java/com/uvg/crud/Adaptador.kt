bb8447a838b826ee0e6f92cbdee1c9371a3668ad
MaIsabelSolano <misolanob@gmail.com>
2021-05-25 11:57:20 -0600

    initial commit

---
+++ app/src/main/java/com/uvg/crud/Adaptador.kt
@@ -0,0 +1,22 @@
+package com.uvg.crud
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class Adaptador : RecyclerView.Adapter<Adaptador.ItemsViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemsViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemsViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    class ItemsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
