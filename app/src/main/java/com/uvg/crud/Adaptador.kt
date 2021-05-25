fd6fe04dffaf7c569cfc939f0ef4a6a6b3ff19b4
MaIsabelSolano <misolanob@gmail.com>
2021-05-25 12:21:08 -0600

    mas

--- app/src/main/java/com/uvg/crud/Adaptador.kt
+++ app/src/main/java/com/uvg/crud/Adaptador.kt
@@ -1,17 +1,21 @@
 package com.uvg.crud
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class Adaptador : RecyclerView.Adapter<Adaptador.ItemsViewHolder>(){
 
+    private var items: MutableList<String> = mutableListOf()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemsViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_lista, parent,false)
+        return ItemsViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return items.size
     }
 
     override fun onBindViewHolder(holder: ItemsViewHolder, position: Int) {
